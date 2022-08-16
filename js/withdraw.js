158002fb20f4c5eb12ffa47133b6166276e60d68
resalatislam5 <resalatislam5@gmail.com>
2022-08-16 06:30:45 +0600

    modify withdraw

--- js/withdraw.js
+++ js/withdraw.js
@@ -1,10 +1,10 @@
 document.getElementById('btn_withdraw').addEventListener('click',function () {
     const userFlild = document.getElementById('user_withdraw');
     const userFlildValue = userFlild.value;
-    const totalDeposit = document.getElementById('total_withdraw');
-    const totalDepositValue = totalDeposit.innerText;
+    const totalWithdraw = document.getElementById('total_withdraw');
+    const totalDepositValue = totalWithdraw.innerText;
     const deposit = parseFloat(userFlildValue) + parseFloat(totalDepositValue);
-    totalDeposit.innerText = deposit;
+    totalWithdraw.innerText = deposit;
     const totalBalanceElement = document.getElementById('total_balance');
     const totalBalanceValue = totalBalanceElement.innerText;
     const totalBalance = parseFloat(totalBalanceValue) - userFlildValue;
