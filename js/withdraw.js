00280421841a52b6a94b4e0605868592974fe358
resalatislam5 <resalatislam5@gmail.com>
2022-08-16 06:48:47 +0600

    withdraw alert

--- js/withdraw.js
+++ js/withdraw.js
@@ -3,10 +3,14 @@ document.getElementById('btn_withdraw').addEventListener('click',function () {
     const userFlildValue = userFlild.value;
     const totalWithdraw = document.getElementById('total_withdraw');
     const totalDepositValue = totalWithdraw.innerText;
-    const deposit = parseFloat(userFlildValue) + parseFloat(totalDepositValue);
-    totalWithdraw.innerText = deposit;
+    const withdraw = parseFloat(userFlildValue) + parseFloat(totalDepositValue);
     const totalBalanceElement = document.getElementById('total_balance');
     const totalBalanceValue = totalBalanceElement.innerText;
+    if(parseFloat(totalBalanceValue) < withdraw){
+        alert('ato tk nai');
+        return;
+    }
+    totalWithdraw.innerText = withdraw;
     const totalBalance = parseFloat(totalBalanceValue) - userFlildValue;
     totalBalanceElement.innerText = totalBalance;
     userFlild.value = '';
